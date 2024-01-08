47e3b645212e3d3237dcaf978210f88d499e4424
markthos <aydenthos@gmail.com>
2024-01-08 15:43:20 -0800

    working on layering on top of the base app

--- src/boutique/views.py
+++ src/boutique/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views import generic
+from oscar.core.loading import get_model
+Boutique = get_model('boutique', 'Boutique')
+class BoutiqueListView(generic.ListView):
+    model = Boutique
+    template_name = 'boutique/boutique_list.html'
+    context_object_name = 'boutique_list'
+class BoutiqueDetailView(generic.DetailView):
+    model = Boutique
+    template_name = 'boutique/boutique_details.html'
+    context_object_name = 'boutique'
