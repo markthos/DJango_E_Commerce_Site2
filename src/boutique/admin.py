e7452e85689b4747da2bf9f16ace628cb0811017
markthos <aydenthos@gmail.com>
2024-01-08 13:29:49 -0800

    setup Boutique app and SuperUser

--- src/boutique/admin.py
+++ src/boutique/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from oscar.core.loading import get_model
+
+Boutique = get_model('boutique', 'Boutique')
 
 # Register your models here.
+
+class BoutiqueAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Boutique, BoutiqueAdmin)
